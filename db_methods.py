a9aba79cca1a827513cb87648c3cdad9bb67d925
Torian <toriancrane@gmail.com>
2017-04-25 09:51:18 -0700

    Fixed threading issue

--- db_methods.py
+++ db_methods.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, MenuItem, Base
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
